f63f3a875b0b47b6c172be84e64e7cec3d8a8c34
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-20 07:18:45 +0800

    Format logs & set level to `debug`

--- src/log/Log.ts
+++ src/log/Log.ts
@@ -1,16 +1,31 @@
-import { createLogger, format } from 'winston';
+import winston from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
-export const Log = createLogger({
-	format: format.combine(
-		format.timestamp({
-			format: 'YYY-MM-DD HH:mm:ss',
+const colorizer = winston.format.colorize();
+export const Log = winston.createLogger({
+	format: winston.format.combine(
+		winston.format.timestamp({
+			format: 'YYYY-MM-DD HH:mm:ss',
 		}),
-		format.errors({ stack: true }),
-		format.splat(),
-		format.json(),
+		winston.format.errors({ stack: true }),
+		winston.format.splat(),
+		winston.format.json(),
 	),
+	level: 'debug',
 	transports: [
+		new winston.transports.Console({
+			format: winston.format.printf((info) =>
+				colorizer.colorize(
+					info.level,
+					`[${String(
+						info.timestamp,
+					)}] [${info.level.toUpperCase()}]: ${info.message}${
+						info.splat == null ? ' ' : `${String(info.splat)}`
+					}`,
+				),
+			),
+			level: 'debug',
+		}),
 		new DailyRotateFile({
 			filename: '%DATE%.log',
 			datePattern: 'YYYY-MM-DD-HH',
@@ -18,6 +33,7 @@ export const Log = createLogger({
 			maxSize: '20m',
 			maxFiles: '14d',
 			dirname: './#logs',
+			level: 'debug',
 		}),
 	],
 });
