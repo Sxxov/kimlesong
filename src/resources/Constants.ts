96ba783e73717903000018ab7911ae70465d0d45
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-21 14:17:07 +0800

    Change embed error strings & remove unneeded

--- src/resources/Constants.ts
+++ src/resources/Constants.ts
@@ -14,11 +14,10 @@ export const enum Constants {
 	PLAYLIST_CONTENT_LIMIT = 100,
 
 	EMBED_COLOUR = '#ff8585',
-	EMBED_AUTHOR_NAME = 'kimlesong',
-	EMBED_AUTHOR_IMAGE = 'https://cdn.discordapp.com/app-icons/889171586760998942/cf3bae7e5db6462329a454dc0ecd3b42.png?size=512',
+	EMBED_AUTHOR_DEFAULT_IMAGE = 'https://cdn.discordapp.com/app-icons/889171586760998942/cf3bae7e5db6462329a454dc0ecd3b42.png?size=512',
 	EMBED_AUTHOR_URL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-	EMBED_TITLE_ERROR_INTERNAL = 'whoops!',
-	EMBED_TITLE_ERROR_USER = 'hmm?',
+	EMBED_TITLE_ERROR_INTERNAL = 'this is awkward—',
+	EMBED_TITLE_ERROR_USER = 'oops!',
 
 	EMBED_BUTTON_QUEUE_NEXT = 'queue.next',
 	EMBED_BUTTON_QUEUE_PREVIOUS = 'queue.previous',
