658e1b2c9c3a93404f00b36c6d75c50322596cc2
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-21 11:46:01 +0800

    Add string constants

--- src/resources/Constants.ts
+++ src/resources/Constants.ts
@@ -17,8 +17,14 @@ export const enum Constants {
 	EMBED_AUTHOR_NAME = 'kimlesong',
 	EMBED_AUTHOR_IMAGE = 'https://cdn.discordapp.com/app-icons/889171586760998942/cf3bae7e5db6462329a454dc0ecd3b42.png?size=512',
 	EMBED_AUTHOR_URL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+	EMBED_TITLE_ERROR_INTERNAL = 'whoops!',
+	EMBED_TITLE_ERROR_USER = 'hmm?',
+
+	EMBED_BUTTON_QUEUE_NEXT = 'queue.next',
+	EMBED_BUTTON_QUEUE_PREVIOUS = 'queue.previous',
 
 	SLASH_ARGUMENT_NAME = 'stuff',
 
 	COMMAND_MORE_TIMEOUT = 60000,
+	COMMAND_QUEUE_PAGE_SIZE = 10,
 }
