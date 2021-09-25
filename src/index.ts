264f2881fbad63ac771cbd0430b74c5df4958ebd
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-26 02:11:49 +0800

    Polyfill `AbortController`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './env.js';
+import AbortController from 'abort-controller';
 import { ClientSingleton } from './client/ClientSingleton.js';
 import { CrashHandlerSingleton } from './client/CrashHandlerSingleton.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+(global as any).AbortController = AbortController;
 CrashHandlerSingleton.register();
 await ClientSingleton.client.login(process.env.TOKEN);
