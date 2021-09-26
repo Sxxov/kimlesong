30b9341e7c9b8bf39b894f870818f63918570693
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-26 17:10:46 +0800

    Use single `VoiceManager` instace per vc state

--- src/state/states/VoiceChannelState.ts
+++ src/state/states/VoiceChannelState.ts
@@ -1,19 +1,23 @@
-import type { Client } from 'discord.js';
+import type { Client, VoiceChannel } from 'discord.js';
 import type { ContinuablePlaylistURL, YoutubeMoosick } from 'youtube-moosick';
 import type { CommandBlueprint } from '../../command/CommandBlueprint.js';
 import type { QueueItem } from '../../queue/QueueItem.js';
 import { ArrayStore } from '../../resources/blocks/classes/store/stores/ArrayStore.js';
+import { VoiceManager } from '../../voice/VoiceManager.js';
 
 export class VoiceChannelState {
 	public id: string;
 	public guildId: string;
 	public client: Client;
 	public ytm: YoutubeMoosick;
+	public previousQueue = new ArrayStore<QueueItem>();
 	public queue = new ArrayStore<QueueItem>();
 	public queuedPlaylists = new ArrayStore<ContinuablePlaylistURL>();
 	public queuedPlaylistsTimeouts = new ArrayStore<
 		ReturnType<typeof setTimeout>
 	>();
+
+	public voiceManager: VoiceManager;
 
 	public lastCommandBlueprint: CommandBlueprint | null = null;
 
@@ -27,5 +31,12 @@ export class VoiceChannelState {
 		this.client = client;
 		this.ytm = ytm;
 		this.guildId = guildId;
+
+		this.voiceManager = new VoiceManager(
+			this.client.guilds.cache
+				.get(this.guildId)
+				?.channels.cache.get(this.id) as VoiceChannel,
+			this,
+		);
 	}
 }
