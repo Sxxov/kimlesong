209eaaa1b80a9a6379396a177e4d9ccf3b7844fa
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-27 05:08:27 +0800

    Pass spotify's credentials to `ClientSingleton`

--- src/worker/ClientWorker.ts
+++ src/worker/ClientWorker.ts
@@ -3,9 +3,17 @@ import type { ClientCredentialsItem } from '../client/ClientCredentialsItem.js';
 import { ClientSingleton } from '../client/ClientSingleton.js';
 
 export class ClientWorker {
-	public static async register(credentials: ClientCredentialsItem) {
-		await ClientSingleton.register(credentials);
+	public static async register(
+		discordCredentials: ClientCredentialsItem,
+		spotifyCredentials: ClientCredentialsItem,
+	) {
+		await ClientSingleton.register(discordCredentials, spotifyCredentials);
 	}
 }
 
-await ClientWorker.register(workerData);
+await ClientWorker.register(
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	workerData.discordCredentials,
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	workerData.spotifyCredentials,
+);
