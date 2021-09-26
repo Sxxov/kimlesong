09af5dcd6c133ccb161285fd4803614ba3b4ba28
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-27 05:09:43 +0800

    Gather & export spotify's credentials to client

--- src/Credentials.ts
+++ src/Credentials.ts
@@ -3,14 +3,26 @@ import * as pathTool from 'path';
 import { fileURLToPath } from 'url';
 import type { ClientCredentialsItem } from './client/ClientCredentialsItem.js';
 
-export const Credentials = JSON.parse(
+export const DiscordCredentials = JSON.parse(
 	(await fs
 		.readFile(
 			pathTool.join(
 				pathTool.dirname(fileURLToPath(import.meta.url)),
-				'../#credentials.json',
+				'../#discord.credentials.json',
 			),
 			{ encoding: 'utf8' },
 		)
-		.catch(() => '')) ?? process.env.CREDENTIALS!,
+		.catch(() => '')) ?? process.env.DISCORD_CREDENTIALS!,
+) as ClientCredentialsItem[];
+
+export const SpotifyCredentials = JSON.parse(
+	(await fs
+		.readFile(
+			pathTool.join(
+				pathTool.dirname(fileURLToPath(import.meta.url)),
+				'../#spotify.credentials.json',
+			),
+			{ encoding: 'utf8' },
+		)
+		.catch(() => '')) ?? process.env.SPOTIFY_CREDENTIALS!,
 ) as ClientCredentialsItem[];
