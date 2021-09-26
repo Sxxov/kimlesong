42df73bccffded8f2038cdfe7ecf65f0c3fbfa48
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-27 05:10:11 +0800

    Add forwarder for `youtube-moosick`'s workers' communication

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,63 @@
 import type { Worker } from 'worker_threads';
-import { Credentials } from './Credentials.js';
-import { ClientWorkerFactory } from './worker/ClientWorkerFactory.js';
+import { DiscordCredentials, SpotifyCredentials } from './Credentials.js';
+import { WorkerFactory } from './worker/WorkerFactory.js';
 import type { ClientCredentialsItem } from './client/ClientCredentialsItem.js';
 import { TrafficResponder } from './traffic/TrafficResponder.js';
+import { AbstractMoosickRequest } from './moosick/requests/AbstractMoosickRequest.js';
+import type { MoosickContinuationResponse } from './moosick/responses/MoosickContinuationResponse.js';
+import type { MoosickMethodResponse } from './moosick/responses/MoosickMethodResponse.js';
+import type { MoosickMethodRequest } from './moosick/requests/MoosickMethodRequest.js';
+import type { MoosickContinuationRequest } from './moosick/requests/MoosickContinuationRequest.js';
 
 export class KimLeSong {
-	private workers: Worker[] = [];
+	private clientWorkers: Worker[] = [];
+	private moosickWorker = new WorkerFactory('MoosickWorker.js').create();
 	private trafficResponder: TrafficResponder;
 
 	constructor() {
-		Credentials.forEach(this.registerWorker.bind(this));
+		DiscordCredentials.forEach(this.registerClientWorker.bind(this));
 
-		this.trafficResponder = new TrafficResponder(this.workers);
+		this.trafficResponder = new TrafficResponder(this.clientWorkers);
 	}
 
-	private registerWorker(credentials: ClientCredentialsItem) {
-		const worker = new ClientWorkerFactory().create(credentials);
+	private registerClientWorker(discordCredentials: ClientCredentialsItem) {
+		const worker = new WorkerFactory('ClientWorker.js').create({
+			discordCredentials,
+			spotifyCredentials: SpotifyCredentials[0],
+		});
 
-		this.workers.push(worker);
+		worker.on('message', (request) => {
+			if (
+				(request as AbstractMoosickRequest)?.type
+				=== AbstractMoosickRequest.TYPE
+			) {
+				const { moosickWorker } = this;
+
+				moosickWorker.postMessage(request);
+				moosickWorker.on(
+					'message',
+					function onMessage(
+						response:
+							| MoosickMethodResponse
+							| MoosickContinuationResponse<any>,
+					) {
+						if (
+							(response as MoosickMethodResponse).id
+								=== (request as MoosickMethodRequest<any>).id
+							|| (response as MoosickContinuationResponse<any>)
+								.methodRequestId
+								=== (request as MoosickContinuationRequest)
+									.methodRequestId
+						) {
+							worker.postMessage(response);
+							moosickWorker.off('message', onMessage);
+						}
+					},
+				);
+			}
+		});
+
+		this.clientWorkers.push(worker);
 	}
 }
 
